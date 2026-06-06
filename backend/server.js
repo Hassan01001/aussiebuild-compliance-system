53ada202d0a6f8c9226766425ce7686a8ea8ebef
Muhammad Hassan Hamayoun <comsatsportssociety@gmail.com>
2026-06-06 13:38:20 +0500

    fixing database issue

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+dns.setDefaultResultOrder('ipv4first');
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -6,7 +10,7 @@ const connectDB = require('./config/db');
 // Load env vars
 dotenv.config();
 
-// Connect to database
+
 connectDB();
 
 const app = express();
